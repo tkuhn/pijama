51fd0cb53f6a903001316673f25176eaf84be58c
Tobias Kuhn <kuhntobias@gmail.com>
2020-11-12 21:26:07 +0100

    Access Pi camera

--- pijama.py
+++ pijama.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 import tkinter as tk
 from tkinter import messagebox
+try:
+	from picamera import PiCamera
+	camera = PiCamera()
+except:
+	print("no picamera")
+from time import sleep
 
 root = tk.Tk()
 root.geometry("800x440")
@@ -14,5 +20,13 @@ def helloCallBack():
 
 button = tk.Button(root, text="Hello", command=helloCallBack)
 button.pack()
+
+def photoCallBack():
+   camera.start_preview()
+   sleep(5)
+   camera.stop_preview()
+
+photoButton = tk.Button(root, text="Photo", command=photoCallBack)
+photoButton.pack()
 
 root.mainloop()
