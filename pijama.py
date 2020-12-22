13150f7c875048780fd236526cec97dcfdd7d525
Tobias Kuhn <kuhntobias@gmail.com>
2020-12-22 14:13:26 +0100

    Finally fix button size issue

--- pijama.py
+++ pijama.py
@@ -35,22 +35,22 @@ def clear():
 def show(path):
   if (path == 'menu'):
     button = tk.Button(frame, text="EXIT", image=images[pixelimgfile], compound="c", command=exit, height=120, width=120, borderwidth=0,
-          bg="#88f", activebackground="#bbf").grid(row=0, column=0, padx=5, pady=5)
+          bg="#88f", activebackground="#bbf", padx=0, pady=0).grid(row=0, column=0, padx=8, pady=8)
   else:
     button = tk.Button(frame, text="<", image=images[pixelimgfile], compound="c", command=partial(show, os.path.dirname(path)), height=120, width=120, borderwidth=0,
-          bg="#88f", activebackground="#bbf").grid(row=0, column=0, padx=5, pady=5)
+          bg="#88f", activebackground="#bbf", padx=0, pady=0).grid(row=0, column=0, padx=8, pady=8)
   col = 1
   row = 0
   for d in sorted([n for n in os.listdir(path) if os.path.isdir(os.path.join(path, n))]):
     chosenDir = os.path.join(path, d)
-    button = tk.Button(frame, text=d, image=images[pixelimgfile], compound="c", command=partial(chosen, chosenDir), height=120, width=120, borderwidth=0,
-        bg="#88f", activebackground="#bbf")
     imgfile = os.path.join(chosenDir, 'img.png')
     if (os.path.isfile(imgfile)):
         images[imgfile] = PhotoImage(file=imgfile)
-        button = tk.Button(frame, text="xxx", image=images[imgfile], command=partial(chosen, chosenDir), height=120, width=120, borderwidth=0,
-            bg="#88f", activebackground="#bbf")
-    button.grid(row=row, column=col, padx=5, pady=5)
+        button = tk.Button(frame, image=images[imgfile], text="", compound="c", command=partial(chosen, chosenDir), height=120, width=120, borderwidth=0,
+            bg="#88f", activebackground="#bbf", padx=0, pady=0).grid(row=row, column=col, padx=8, pady=8)
+    else:
+        button = tk.Button(frame, image=images[pixelimgfile], text=d, compound="c", command=partial(chosen, chosenDir), height=120, width=120, borderwidth=0,
+    	    bg="#88f", activebackground="#bbf", padx=0, pady=0).grid(row=row, column=col, padx=8, pady=8)
     col = col + 1
     if (col == 5):
       row = row + 1
